de564921decd8a48087f360b2ed0a844c4370f06
Jesse Manninen <jesse.manninen@gmail.com>
2024-04-20 15:04:06 +0300

    t3-oss env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
-import "dotenv/config";
-
 import { defineConfig } from "drizzle-kit";
+import { env } from "./src/env";
 
 export default defineConfig({
   schema: "./src/drizzle/schema.ts",
   out: "./migrations",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.POSTGRES_URL! + "?sslmode=require",
+    connectionString: env.POSTGRES_URL + "?sslmode=require",
   },
   verbose: true,
   strict: true,
