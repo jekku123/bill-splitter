f77975e5ca5fa4c8bec894c85d2241a5a49903a2
Jesse Manninen <jesse.manninen@gmail.com>
2024-04-20 13:53:08 +0300

    e2e status badge to readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bill Splitter ![Vercel Deploy](https://therealsujitk-vercel-badge.vercel.app/?app=bill-splitter-topaz&style=for-the-badge)
+# Bill Splitter ![Vercel Deploy](https://therealsujitk-vercel-badge.vercel.app/?app=bill-splitter-topaz&style=for-the-badge) [![End-to-end tests](https://github.com/jekku123/bill-splitter/actions/workflows/main.yml/badge.svg)](https://github.com/jekku123/bill-splitter/actions/workflows/main.yml)
 
 App to keep track of the expenses of a group of people and the debts between them during trips, dinner meetings, etc.
 
@@ -17,5 +17,6 @@ App to keep track of the expenses of a group of people and the debts between the
 - Drizzle
 - NextAuth.js
 - Zod
+- Cypress
 
 [Live demo](http://bill-splitter-topaz.vercel.app)
--- cypress/e2e/authentication.cy.ts
+++ cypress/e2e/authentication.cy.ts
@@ -9,34 +9,12 @@
 //   });
 // };
 
-describe("Register and login", () => {
-  beforeEach(() => {
-    cy.visit("/");
-  });
-
-  it("Check if the app has a register button", () => {
-    cy.get('[data-test-id="register-button"]')
-      .should("be.visible")
-      .should("exist");
-    cy.get('[data-test-id="register-button"]').should("have.text", "Register");
-  });
-
-  it("Check if the app has a login button", () => {
-    cy.get('[data-test-id="login-button"]')
-      .should("be.visible")
-      .should("exist");
-    cy.get('[data-test-id="login-button"]').should("have.text", "Login");
-  });
-
-  it("Click on register button", () => {
-    cy.get('[data-test-id="register-button"]').click();
-    cy.url().should("eq", "http://localhost:3000/register");
-  });
-});
-
 describe("Login form tests", () => {
-  it("Test with wrong credentials", () => {
+  beforeEach(() => {
     cy.visit("/login");
+  });
+
+  it("Test with wrong credentials", () => {
     cy.get("[data-test-id=login-email]").type("test@test.com");
     cy.get("[data-test-id=login-password]").type("nottherightpassword");
     cy.get("[data-test-id=login-submit]").click();
@@ -47,7 +25,6 @@ describe("Login form tests", () => {
   });
 
   it("Test with empty credentials", () => {
-    cy.visit("/login");
     cy.get("[data-test-id=login-submit]").click();
     cy.get('[data-test-id="login-form"]').should(
       "contain.text",
@@ -60,7 +37,6 @@ describe("Login form tests", () => {
   });
 
   it("Test with empty password", () => {
-    cy.visit("/login");
     cy.get("[data-test-id=login-email]").type("test@test.com");
     cy.get("[data-test-id=login-submit]").click();
     cy.get('[data-test-id="login-form"]').should(
@@ -74,7 +50,6 @@ describe("Login form tests", () => {
   });
 
   it("Test with empty email", () => {
-    cy.visit("/login");
     cy.get("[data-test-id=login-password]").type("password123");
     cy.get("[data-test-id=login-submit]").click();
     cy.get('[data-test-id="login-form"]').should(
@@ -88,7 +63,6 @@ describe("Login form tests", () => {
   });
 
   it("Test with invalid email", () => {
-    cy.visit("/login");
     cy.get("[data-test-id=login-email]").type("test");
     cy.get("[data-test-id=login-password]").type("password123");
     cy.get("[data-test-id=login-submit]").click();
@@ -103,7 +77,6 @@ describe("Login form tests", () => {
   });
 
   it("Test with correct credentials", () => {
-    cy.visit("/login");
     cy.get("[data-test-id=login-email]").type("test@test.com");
     cy.get("[data-test-id=login-password]").type("password123");
     cy.get("[data-test-id=login-submit]").click();
@@ -112,9 +85,11 @@ describe("Login form tests", () => {
 });
 
 describe("Register form tests", () => {
-  it("Try registering with an existing email", () => {
+  beforeEach(() => {
     cy.visit("/register");
-    cy.get("[data-test-id=register-email]").type("keke@keke.com");
+  });
+  it("Try registering with an existing email", () => {
+    cy.get("[data-test-id=register-email]").type("test@test.com");
     cy.get("[data-test-id=register-password]").type("password123");
     cy.get("[data-test-id=register-confirm-password]").type("password123");
     cy.get("[data-test-id=register-submit]").click();
@@ -125,7 +100,6 @@ describe("Register form tests", () => {
   });
 
   it("Try registering with an invalid email", () => {
-    cy.visit("/register");
     cy.get("[data-test-id=register-email]").type("keke");
     cy.get("[data-test-id=register-password]").type("password123");
     cy.get("[data-test-id=register-confirm-password]").type("password123");
@@ -137,7 +111,6 @@ describe("Register form tests", () => {
   });
 
   it("Try registering with an empty email", () => {
-    cy.visit("/register");
     cy.get("[data-test-id=register-password]").type("password123");
     cy.get("[data-test-id=register-confirm-password]").type("password123");
     cy.get("[data-test-id=register-submit]").click();
@@ -148,8 +121,7 @@ describe("Register form tests", () => {
   });
 
   it("Try registering with an empty password", () => {
-    cy.visit("/register");
-    cy.get("[data-test-id=register-email]").type("keke@keke.com");
+    cy.get("[data-test-id=register-email]").type("test@test.com");
     cy.get("[data-test-id=register-submit]").click();
     cy.get('[data-test-id="register-form"]').should(
       "contain.text",
@@ -158,8 +130,7 @@ describe("Register form tests", () => {
   });
 
   it("Try registering with an empty confirm password", () => {
-    cy.visit("/register");
-    cy.get("[data-test-id=register-email]").type("keke@keke.com");
+    cy.get("[data-test-id=register-email]").type("test@test.com");
     cy.get("[data-test-id=register-password]").type("password123");
     cy.get("[data-test-id=register-submit]").click();
     cy.get('[data-test-id="register-form"]').should(
@@ -168,9 +139,8 @@ describe("Register form tests", () => {
     );
   });
 
-  it("Try registering with a password that doesn't match", () => {
-    cy.visit("/register");
-    cy.get("[data-test-id=register-email]").type("keke@keke.com");
+  it("Try registering with a password that doesn't match with confirm-password", () => {
+    cy.get("[data-test-id=register-email]").type("test@test.com");
     cy.get("[data-test-id=register-password]").type("password123");
     cy.get("[data-test-id=register-confirm-password]").type("password1234");
     cy.get("[data-test-id=register-submit]").click();
